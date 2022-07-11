57172bbfe751abd7d5642272e5d46e3cf7f4a10c
Mihai Vasilian <grayasm@gmail.com>
2022-07-11 12:41:17 +0200

    Initialize v.2022-06-19_1720

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "v.2022-06-19_1720");
+    printf("%s\n", "changed at 2022-06-19_1727");
+    printf("%s\n", "changed at 2022-06-19_1728");
+    printf("%s\n", "changed at 2022-06-19_1758");
+    return 0;
+}
