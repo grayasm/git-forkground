2692282bc66a7ab9014d734316210403064e3412
Mihai Vasilian <grayasm@gmail.com>
2022-07-11 15:44:05 +0200

    change - 2022-07-11_1543

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     printf("%s\n", "v.2022-07-11_1520");
     throw "bad - 2022-07-11_1541";
     printf("%s\n", "change - 2022-07-11_1542");
+    printf("%s\n", "change - 2022-07-11_1543");
     return 0;
 }
