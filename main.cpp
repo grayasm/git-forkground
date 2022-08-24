dc84a5dd3722c67538c3c5fbc8e0af0841ea7592
mihai <mihai@binary.fritz.box>
2022-08-24 14:48:04 +0200

    changed at 2022-08-24_1447

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("%s\n", "v.2022-06-19_1015");
-    printf("%s\n", "changed at 2022-06-22_1405");
+    printf("%s\n", "v.2022-08-24_1440");
+    printf("%s\n", "changed at 2022-08-24_1447");
     return 0;
 }
