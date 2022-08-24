65da2cf11a8df53403ecc8d5ecf3980205413594
Mihai Vasilian <grayasm@gmail.com>
2022-08-24 14:59:37 +0200

    changed at 2022-08-24_1458

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     printf("%s\n", "v.2022-08-24_1440");
     printf("%s\n", "changed at 2022-08-24_1447");
     printf("%s\n", "changed at 2022-08-24_1454"); // should tag this commit...
+    printf("%s\n", "changed at 2022-08-24_1458");
     return 0;
 }
