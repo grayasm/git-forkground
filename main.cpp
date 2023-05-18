603c335a2f75594d3e13dabea3815177883a17f2
Mihai Vasilian <grayasm@gmail.com>
2023-05-18 09:38:10 +0200

    2nd change

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main()
 {
   printf("%s\n", "main at 2023-05-18 09:29:00");
   printf("%s\n", "1st modification 2023-05-18 09:36:51");
+  printf("%s\n", "2nd modification 2023-05-18 09:37:50");
   return 0;
 }
